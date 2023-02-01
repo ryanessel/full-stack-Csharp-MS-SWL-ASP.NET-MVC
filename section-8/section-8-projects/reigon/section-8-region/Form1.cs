17b4cf42b4907078e9e0b7dea40d19bdc74482fe
Ry Work <spucti19@gmail.com>
2023-02-01 13:49:15 -0500

    section-8 region complete

---
+++ section-8/section-8-projects/reigon/section-8-region/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace section_8_region
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //RED REGION
+            #region
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text"; label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text"; label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+            label1.Text = "text";
+
+
+            #endregion
+
+
+        }
+    }
+}
--- section-8/using-region.md
+++ section-8/using-region.md
@@ -1,1 +1,11 @@
 Reigon
+- a directive that partitions your code.
+
+- this allows us to exapand or collaspse sections for readability
+
+
+#region
+
+Code Block
+
+#endregion
